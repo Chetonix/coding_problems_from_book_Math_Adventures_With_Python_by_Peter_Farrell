e425dcfd6ebeffd69a44c4ad64e66548ec25c344
chetanharjani <chetonix@gmail.com>
2024-04-02 13:18:59 +0530

    add two files for chapter 4

---
+++ Ex4_1.py
@@ -0,0 +1,7 @@
+def equation(a, b, c, d):
+    return (d-b) / (a-c)
+
+
+if __name__ == '__main__':
+    print(equation(12, 18 , -34, 67))
+    print(equation(2, 5, 0, 13))
---
+++ Ex4_2.py
@@ -0,0 +1,3 @@
+from Ex4_1 import equation
+
+print(equation(1/2, 2/3, 1/5, 7/8))
